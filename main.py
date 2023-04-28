78bbd13477373f6428219824184c9ef720c9ec4e
AlinaOs <47086948+AlinaOs@users.noreply.github.com>
2023-04-28 21:49:55 +0200

    Implement text segmentation by sentences

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from preprocessing.prep import TextExtractor, TextCleaner
+from preprocessing.prep import TextExtractor, TextCleaner, TextSegmenter
 
 
 fulltextDk = 'fulltexts/Dk_complete_tei.xml'
@@ -9,14 +9,23 @@ metaDkCSV = 'input/Dk_Meta.csv'
 # pagesKChCSV = 'input/KCh_Pages.csv'
 
 normalize = 'input/normalization.csv'
+normalize_meta = 'input/normalization_meta.csv'
 cleanPagesDkCSV = 'input/DK_Pages_Clean.csv'
 cleanMetaDkCSV = 'input/DK_Meta_Clean.csv'
 # cleanPagesKChCSV = 'input/KCh_Pages_Clean.csv'
 # cleanMetaKChCSV = 'input/KCh_Meta_Clean.csv'
 
+sentencesDkCSV = 'input/DK_sentences.csv'
+sentencesMetaDkCSV = 'input/DK_Meta_sentences.csv'
+# ngramsDkCSV = 'input/DK_sentences.csv'
+
 TE = TextExtractor()
 TC = TextCleaner()
+TS = TextSegmenter()
 
 TE.extractDkText(fulltextDk, pagesDkCSV, metaDkCSV)
 TC.cleanTextFromCSV(pagesDkCSV, cleanPagesDkCSV, normalize=normalize)
-TC.cleanTextFromCSV(metaDkCSV, cleanMetaDkCSV, normalize=normalize)
+TC.cleanTextFromCSV(metaDkCSV, cleanMetaDkCSV, normalize=normalize_meta)
+TS.segmentBySentences(cleanPagesDkCSV, sentencesDkCSV)
+TS.segmentBySentences(cleanMetaDkCSV, sentencesMetaDkCSV)
+
--- preprocessing/prep.py
+++ preprocessing/prep.py
@@ -107,6 +107,13 @@ class TextExtractor:
                         if pageLines:
                             joined = self.joinLines(pageLines)
                             if joined != '':
+                                # re-add syllabification marker if a word is broken over two pages
+                                li = -1
+                                if len(pageLines[-1].findall('./' + self.tei + 'fw[@type="sig"]')) > 0:
+                                    li = -2
+                                if len(pageLines[li].findall('./' + self.tei + 'pc[@type="syllabification"]')) > 0:
+                                    joined += '-'
+
                                 pagesText.append([
                                     joined,
                                     bookpart,
@@ -151,6 +158,13 @@ class TextExtractor:
                 if pageLines:
                     joined = self.joinLines(pageLines)
                     if joined != '':
+                        # re-add syllabification marker if a word is broken over two pages
+                        li = -1
+                        if len(pageLines[-1].findall('./' + self.tei + 'fw[@type="sig"]')) > 0:
+                            li = -2
+                        if len(pageLines[li].findall('./' + self.tei + 'pc[@type="syllabification"]')) > 0:
+                            joined += '-'
+
                         pagesText.append([
                             joined,
                             bookpart,
@@ -183,11 +197,11 @@ class TextExtractor:
 # ALL TEXTS ###
 class TextCleaner:
 
-    def cleanTextFromCSV(self, inputpath, outputpath, normalize=None):
-        csv = readFromCSV(inputpath)
+    def cleanTextFromCSV(self, input, output, normalize=None):
+        csv = readFromCSV(input)
         for l in csv['lines']:
             l[0] = self.cleanText(l[0], normalize)
-        writeToCSV(outputpath, csv['header'], csv['lines'])
+        writeToCSV(output, csv['header'], csv['lines'])
 
     def cleanText(self, text: str, normalize=None):
         """
@@ -274,7 +288,141 @@ class TextCleaner:
 
 
 class TextSegmenter:
-    pass
-    # TODO Segment text
+    INSERTION = {'sentbound', 'join', 'ignore'}
+    """
+    sentbound: Treat insertion markers like sentence boundaries
+    ignore: Ignore insertion markers and treat insertions as part of the text surrounding them.
+    join: Create two segments: One ignoring the insertion markers and an additional segment ignoring the insertion text.
+    """
+
+    def segmentsToList(self, parsegments):
+        locatedSegments = []
+        for seg in parsegments:
+            segList = [seg[0]]
+            segList += seg[1]
+            locatedSegments.append(segList)
+        return locatedSegments
+
+    def segmentBySentences(self, innput, output, minwords=4, insertion='join'):
+        """
+        Segments the text of input into sentences and saves the resulting segments with their corresponding
+        text locations to output.
+        Sentence boundaries must be marked by '#SEND#' or '#CSTART#' in the input file. If a sentence has less than
+        minwords, it is considered part of the sentence before or, if there isn't a sentence before it, the sentence
+        after it. If a sentence has one or two words, it is considered an enumeration and gets joined with both the
+        previous and next sentence.
+        Depending on the value of insertion, insertions will be treated as sentence ends, ignored or added.
+
+        :param innput: The path to the input file.
+        :param output: The path to where the output shall be written.
+        :param minwords: Minimal number of words in a sentence.
+        :param insertion: Treat insertions as sentence ends ('sentbound'), 'ignore' them, or add a sentence ignoring the insertion ('join').
+        """
+        csv = readFromCSV(innput)
+
+        split = r'\s*(?:#SEND#)|(?:#CSTART#)\s*'
+        if insertion == r'sentbound':
+            split = r'\s*(?:#SEND#)|(?:#CSTART#)|(?:#INSTART#)|(?:#INEND#)\s*'
+
+        sentences = []
+        parsentences = []
+        pendingsent = None
+        location = ['', '', '', '', '']
+
+        for l in csv['lines']:
+            if (l[1] != location[0] or l[2] != location[1]) and pendingsent is not None:
+                if int(pendingsent[1]) < minwords and len(parsentences) > 0:
+                    last = parsentences.pop()
+                    leftover = ' '.join([last[0], pendingsent[0].strip()])
+                    if pendingsent[1] > len(last[0].split()):
+                        location = pendingsent[2]
+                    else:
+                        location = last[1]
+                else:
+                    leftover = pendingsent[0].strip()
+
+                parsentences.append((leftover, location))
+                pendingsent = None
+
+            sentences.extend(self.segmentsToList(parsentences))
+            parsentences = []
+            location = l[1:]
+
+            text = l[0].strip()
+            if text == '':
+                continue
+            if insertion == 'ignore':
+                re.sub(r'\s*(?:#INSTART#|#INEND#)\s*', ' ', text)
+
+            if insertion == 'join':
+                # TODO: Check insertion
+                pass
+
+            text = re.split(split, text.strip())
+            for sent in text:
+                sentloc = location
+                sent = sent.strip()
+                if sent == '':
+                    continue
+
+                words = sent.split()
+                if pendingsent is not None:
+                    if pendingsent[0].endswith('-'):
+                        sent = pendingsent[0].strip()[0:-1] + sent
+                    else:
+                        sent = pendingsent[0].strip() + ' ' + sent
+                    if pendingsent[1] > len(words):
+                        sentloc = pendingsent[2]
+                    pendingsent = None
+
+                words = sent.split()
+                # Check for minimal length
+                if len(words) < minwords:
+                    if len(parsentences) > 0:
+                        if len(words) < 3:
+                            # less than 3 words -> enumeration, connect to previous AND next sentence
+                            newpend = parsentences.pop()[0] + ' ' + sent
+                            pendingsent = (newpend, len(newpend), location)
+                        else:
+                            parsentences.append((parsentences.pop()[0]+' '+sent, sentloc))
+                    else:
+                        pendingsent = (sent, len(words), location)
+                else:
+                    parsentences.append((sent, sentloc))
+
+            # Check for sentences overlapping the page
+            if pendingsent is None and re.search(r'#SEND#\.*$', l[0]) is None:
+                sent = parsentences.pop()[0]
+                words = len(sent.split())
+                pendingsent = (sent, words, location)
+
+        if int(pendingsent[1]) < minwords and len(parsentences) > 0:
+            last = parsentences.pop()
+            leftover = ' '.join([last[0], pendingsent[0].strip()])
+            if pendingsent[1] > len(last[0].split()):
+                location = pendingsent[2]
+            else:
+                location = last[1]
+        else:
+            leftover = pendingsent[0].strip()
+
+        parsentences.append((leftover, location))
+        sentences.extend(self.segmentsToList(parsentences))
+
+        writeToCSV(output, csv['header'], sentences)
+
+    def segmentByNGrams(self, innput, output, n=5, insertion='join', ignoresentbounds=True):
+        """
+
+
+        :param innput:
+        :param output:
+        :param n: Width of the token window (the n in n-gram).
+        :param insertion:
+        :param ignoresentbounds: May n-grams overlap sentence boundaries?
+        :return:
+        """
+        pass
+
 
 # TODO Prepare TRD input files
